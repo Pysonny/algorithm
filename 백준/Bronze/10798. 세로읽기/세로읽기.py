8f077edd7fb8e328618d061d0457619b7d6fcfd1
Gyesu_Choi <111727992+Pysonny@users.noreply.github.com>
2023-01-31 17:09:15 +0900

    [Bronze I] Title: 세로읽기, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10798. 세로읽기/세로읽기.py
@@ -0,0 +1,9 @@
+matrix = [input() for _ in range(5)]
+
+for i in range(15):
+    for j in range(15):
+        try:
+            print(matrix[j][i],end='')
+        except:
+            continue 
+print()
