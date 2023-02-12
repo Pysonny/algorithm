f58bde9fc176c102750b6313aa132ab809d4ca27
Gyesu_Choi <111727992+Pysonny@users.noreply.github.com>
2023-02-12 14:47:23 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 944 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
@@ -1,10 +1,4 @@
-T = int(input())
-
-
-for t in range(1,T+1):
-    sen = input().split()
-    sen2 =[]
-    for i in sen:
-        i = i[::-1]
-        sen2.append(i)
-    print(*sen2)
+for _ in range(int(input())):
+    S = input().split()
+    for i in S:
+        print(i[::-1],end=' ')
